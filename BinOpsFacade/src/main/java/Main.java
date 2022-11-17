ac6b8d319062f1edf004d6a97b86bdd7431178ee
Andrey Kolesnik <grafalastormusic@mail.ru>
2022-11-17 17:21:44 +0300

    Initial commit

---
+++ BinOpsFacade/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("10101", "1011"));
+        System.out.println(binOps.mult("111", "100"));
+    }
+}
